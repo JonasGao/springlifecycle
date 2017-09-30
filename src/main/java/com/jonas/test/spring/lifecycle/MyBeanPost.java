eb13f8087931f2b6e7ef2e8851966aa0ab65ed1a
Jonas Gao <gaopinsong@live.cn>
2017-10-01 00:55:02 +0800

    init

---
+++ src/main/java/com/jonas/test/spring/lifecycle/MyBeanPost.java
@@ -0,0 +1,16 @@
+package com.jonas.test.spring.lifecycle;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class MyBeanPost implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
+        return o;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
+        return o;
+    }
+}
