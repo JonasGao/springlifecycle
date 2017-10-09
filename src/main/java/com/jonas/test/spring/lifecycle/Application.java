2e6dca00f194f2eb13a0d8121e2a9c3e57147346
JonasGao <jonas.gao@voyageone.cn>
2017-10-09 13:06:03 +0800

    add CommandLineRunner

--- src/main/java/com/jonas/test/spring/lifecycle/Application.java
+++ src/main/java/com/jonas/test/spring/lifecycle/Application.java
@@ -2,6 +2,10 @@ package com.jonas.test.spring.lifecycle;
 
 import com.other.test.EnableMyImportAware;
 import com.other.test.MyImportBeanDefinitionRegistrar;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -13,10 +17,25 @@ import static com.jonas.test.spring.lifecycle.Log.println;
 @Import(MyImportBeanDefinitionRegistrar.class)
 @EnableMyImportAware
 @EnableConfigurationProperties(MyImportProperties.class)
-public class Application {
+public class Application implements CommandLineRunner {
+
     public static void main(String[] args) {
         println("SpringApplication", "run", "开始");
         SpringApplication.run(Application.class, args);
         println("SpringApplication", "run", "结束");
+    }
+
+    @Autowired(required = false)
+    private BeanDefinitionRegistry beanDefinitionRegistry;
+
+    @Autowired
+    private ConfigurableListableBeanFactory configurableListableBeanFactory;
+
+    @Override
+    public void run(String... args) throws Exception {
+        println("Application (CommandLineRunner)", "run");
+
+        System.out.println(beanDefinitionRegistry);
+        System.out.println(configurableListableBeanFactory);
     }
 }
