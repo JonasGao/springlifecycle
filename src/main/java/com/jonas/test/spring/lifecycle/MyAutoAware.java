d4f7bad7a4087c8200cb4a6befd4fef32b316a8a
JonasGao <jonas.gao@voyageone.cn>
2017-10-09 12:44:53 +0800

    add MyAutoAware

--- src/main/java/com/jonas/test/spring/lifecycle/MyAutoAware.java
+++ src/main/java/com/jonas/test/spring/lifecycle/MyAutoAware.java
@@ -1,10 +1,13 @@
 package com.jonas.test.spring.lifecycle;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
@@ -12,7 +15,11 @@ import org.springframework.stereotype.Component;
 import static com.jonas.test.spring.lifecycle.Log.println;
 
 @Component
+@EnableConfigurationProperties(AutoAwareProperties.class)
 public class MyAutoAware implements BeanFactoryPostProcessor, BeanDefinitionRegistryPostProcessor, ApplicationContextAware {
+
+    @Autowired
+    private AutoAwareProperties properties;
 
     @Override
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
@@ -27,5 +34,32 @@ public class MyAutoAware implements BeanFactoryPostProcessor, BeanDefinitionRegi
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         println("MyAutoAware", "setApplicationContext", applicationContext.getClass().getName() + "; " + applicationContext.getId());
+        println("MyAutoAware", "autowired AutoAwareProperties", String.valueOf(properties));
+    }
+}
+
+@ConfigurationProperties("com.test.auto")
+class AutoAwareProperties {
+
+    public AutoAwareProperties() {
+        println("AutoAwareProperties", "constructor");
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        println("AutoAwareProperties", "setName");
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "AutoAwareProperties{" +
+                "name='" + name + '\'' +
+                '}';
     }
 }
