99a1db6f08313d40a0257611bf86edfffa16a390
Jonas <jonas.gao@voyageone.cn>
2019-06-11 11:13:57 +0800

    switch project structure

---
+++ simple/src/main/java/com/jonas/test/spring/onerequest/TheLifeOfARequestApplication.java
@@ -0,0 +1,11 @@
+package com.jonas.test.spring.onerequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLifeOfARequestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TheLifeOfARequestApplication.class);
+    }
+}
